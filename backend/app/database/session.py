7ccf43b87b76ac66c9e85c33c372e34643484c61
KWSMooBang <40kimwoosung@gmail.com>
2025-09-23 23:32:42 +0900

    Implement get_db_session function

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -1,11 +1,29 @@
+from typing import Generator, Any
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
 from app.core.config import settings
 
+
 engine = create_engine(
-    settings.DATABASE_URL,
-    pool_pre_ping=True,
-    pool_recycle=3600,
-    future=True,
+    settings.DATABASE_URL, 
+    pool_pre_ping=True, 
+    pool_recycle=3600, 
+    future=True
 )
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+SessionLocal = sessionmaker(
+    bind=engine, 
+    autoflush=False, 
+    expire_on_commit=False,
+    future=True
+) 
+
+def get_db_session() -> Generator[Session, Any, None]:
+    session = SessionLocal()
+    try:
+        yield session
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        raise e
+    finally:
+        session.close()
