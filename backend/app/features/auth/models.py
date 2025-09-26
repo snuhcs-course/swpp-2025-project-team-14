a87b55d9bef81ecdc93186e14c347d002bf6075c
KWSMooBang <40kimwoosung@gmail.com>
2025-09-26 16:16:52 +0900

    feat: create BlockedToken model and move User model

--- backend/app/features/auth/models.py
+++ backend/app/features/auth/models.py
@@ -1,12 +1,10 @@
-from sqlalchemy import String, Integer
+from datetime import datetime
+from sqlalchemy import String, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database.base import Base
-
-
-class User(Base):
-    __tablename__ = 'users'
     
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    login_id: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    hashed_password: Mapped[str] = mapped_column(String(100), nullable=False)
-    username: Mapped[str] = mapped_column(String(100), nullable=True)
+class BlockedToken(Base):
+    __tablename__ = 'blocked_tokens'
+    
+    token_id: Mapped[str] = mapped_column(String(255), primary_key=True)
+    expired_at: Mapped[datetime] = mapped_column(DateTime)
---
+++ backend/app/features/user/models.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+from app.database.base import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    login_id: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(100), nullable=False)
+    username: Mapped[str] = mapped_column(String(100), nullable=True)
