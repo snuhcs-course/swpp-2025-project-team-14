8cfaf6bb9fedb220addd751f5c4bd30e741547b1
KWSMooBang <40kimwoosung@gmail.com>
2025-09-26 16:39:31 +0900

    feat: create repository for User, BlockedToken models

---
+++ backend/app/features/auth/repository.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+from datetime import datetime
+from fastapi import Depends
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from app.database.session import get_db_session
+from .models import BlockedToken
+
+
+class BlockedTokenRepository:
+    def __init__(self, session: Annotated[Session, Depends(get_db_session)]) -> None:
+        self.session = session
+
+    def add_blocked_token(self, token_id: str, expired_at: datetime) -> BlockedToken:
+        blocked_token = BlockedToken(token=token_id, expired_at=expired_at)
+        self.session.add(blocked_token)
+        self.session.commit()
+        self.session.refresh(blocked_token)
+        return blocked_token
+
+    def is_token_blocked(self, token_id: str) -> bool:
+        print(token_id)
+        return self.session.scalar(
+            select(BlockedToken).filter(BlockedToken.token == token_id)
+        ) is not None
