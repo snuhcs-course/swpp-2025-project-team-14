bb6b8b1e3ed067d94d247298c22d6a8e72d327dc
taehyunlee <thl012@snu.ac.kr>
2025-10-16 03:04:21 +0900

    fix: changed question generating process to two step

--- backend/app/features/selfaware/service.py
+++ backend/app/features/selfaware/service.py
@@ -6,20 +6,21 @@ from langchain_core.output_parsers import StrOutputParser
 from app.database.crud import question_crud
 from app.database.schemas import question_schema as schema
 from sqlalchemy.orm import Session
+from langchain.schema.runnable import RunnableMap
+from .prompt import emotion_prompt, question_prompt
 
 def generate_selfaware_question(db: Session, journal_content: str, user_id: int):
     llm = ChatOpenAI(model="gpt-5-nano")
     output_parser = StrOutputParser()
-    prompt = ChatPromptTemplate.from_template("""
-    사용자의 일기 내용이 주어집니다. 
-    이 내용을 바탕으로 사용자가 스스로를 성찰할 수 있도록 돕는 질문을 1개 생성하세요.
 
-    일기 내용:
-    {journal}
-    """)
+    emotion_chain = emotion_prompt | llm
+    question_chain = question_prompt | llm
 
-    chain = prompt | llm | output_parser
-    response = chain.invoke({"journal": journal_content})
+    reflection_agent = RunnableMap({
+        "analysis": emotion_chain,
+    }) | question_chain | output_parser
+
+    response = reflection_agent.invoke({"journal": journal_content})
 
     # DB에 저장
     question_data = schema.QuestionCreate(
