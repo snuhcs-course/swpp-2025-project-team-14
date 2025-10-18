ec0413e894509841b51892468a2fd455ce0f79cb
SHPark524 <twinpsh1@gmail.com>
2025-10-19 06:06:42 +0900

    feat: journal write feature, fullscreen

--- android/app/src/main/java/com/example/mindlog/JournalWriteActivity.kt
+++ android/app/src/main/java/com/example/mindlog/JournalWriteActivity.kt
@@ -1,19 +1,85 @@
 package com.example.mindlog
 
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
-class JournalWriteActivity : AppCompatActivity() {
+class JournalWriteActivity : BaseActivity() {
+
+    private val emotionSelectFragment = EmotionSelectFragment()
+    private val contentWriteFragment = ContentWriteFragment()
+
+    private lateinit var btnCancelOrBack: Button
+    private lateinit var btnNextOrSave: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_journal_write)
 
-        // Activity가 처음 생성될 때만 초기 프래그먼트(감정 선택 화면)를 표시합니다.
-        // 화면 회전 등 상태 변경 시에는 프래그먼트가 중복으로 추가되는 것을 방지합니다.
+        btnCancelOrBack = findViewById(R.id.btn_cancel_or_back)
+        btnNextOrSave = findViewById(R.id.btn_next_or_save)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.write_fragment_container, EmotionSelectFragment())
+                .replace(R.id.write_fragment_container, emotionSelectFragment)
                 .commit()
+            updateButtonsForEmotionSelect()
         }
+
+        setupButtonClickListeners()
+
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val currentFragment = supportFragmentManager.findFragmentById(R.id.write_fragment_container)
+                if (currentFragment is ContentWriteFragment) {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.write_fragment_container, emotionSelectFragment)
+                        .commit()
+                    updateButtonsForEmotionSelect()
+                } else {
+                    finish()
+                }
+            }
+        }
+        onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    private fun setupButtonClickListeners() {
+        btnCancelOrBack.setOnClickListener {
+            val currentFragment = supportFragmentManager.findFragmentById(R.id.write_fragment_container)
+            if (currentFragment is ContentWriteFragment) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.write_fragment_container, emotionSelectFragment)
+                    .commit()
+                updateButtonsForEmotionSelect()
+            } else {
+                finish()
+            }
+        }
+
+        btnNextOrSave.setOnClickListener {
+            val currentFragment = supportFragmentManager.findFragmentById(R.id.write_fragment_container)
+            if (currentFragment is EmotionSelectFragment) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.write_fragment_container, contentWriteFragment)
+                    .commit()
+                updateButtonsForContentWrite()
+            } else {
+                // TODO: 여기에 작성된 일기 데이터를 데이터베이스에 저장하는 로직을 추가해야 합니다.
+                finish()
+            }
+        }
+    }
+
+    private fun updateButtonsForEmotionSelect() {
+        btnCancelOrBack.text = "취소"
+        btnNextOrSave.text = "다음"
+    }
+
+    private fun updateButtonsForContentWrite() {
+        btnCancelOrBack.text = "뒤로"
+        btnNextOrSave.text = "작성"
     }
 }
