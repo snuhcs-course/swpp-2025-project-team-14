9490d07f801b9beabed1facc942bfda26ad9f6fb
KWSMooBang <40kimwoosung@gmail.com>
2025-10-21 21:21:56 +0900

    feat: implement basic UIs for selfaware features

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -57,9 +57,12 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.google.android.material:material:1.12.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.activity:activity-ktx:1.9.0")
