cd9ada8ea466245a01a4f520ff59d88c77f369a6
taehyunlee <thl012@snu.ac.kr>
2025-11-12 13:06:32 +0900

    feat: add analysis router

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,7 @@ from .features.journal.router import router as journal_router
 from .features.statistics.router import router as statistics_router
 from .features.user.router import router as user_router
 from .features.selfaware.router import router as self_aware_router
+from .features.analysis.router import router as analysis_router
 
 app = FastAPI(title="MindLog")
 
@@ -12,8 +13,8 @@ app.include_router(auth_router, prefix="/api/v1")
 app.include_router(user_router, prefix="/api/v1")
 app.include_router(journal_router, prefix="/api/v1")
 app.include_router(statistics_router, prefix="/api/v1")
-
 app.include_router(self_aware_router, prefix="/api/v1")
+app.include_router(analysis_router, prefix="/api/v1")
 
 @app.get("/")
 def read_root():
